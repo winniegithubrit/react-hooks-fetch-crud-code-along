6fc14f8e10362b312a42220cc7de2c3a63854766
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-04-25 11:52:04 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,44 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item,onUpdateItem ,onDeleteItem}) {
+  function handleDeleteClick() {
+    console.log(item);
+  }
+function handleAddToCartClick(){
+ // console.log("clicked item:",item)
+ fetch(`http://localhost:4000/items/${item.id}`,{
+  method:"PATCH",
+  headers:{
+    "Content-Type":"application/json",
+  },
+  body:JSON.stringify({
+    isInCart:!item.isInCart
+  })
+ })
+ .then((response)=>response.json())
+ .then((updatedItem)=>onUpdateItem(updatedItem))
+}
+//delete request
+function handleDeleteClick() {
+  fetch(`http://localhost:4000/items/${item.id}`, {
+    method: "DELETE",
+  })
+    .then((r) => r.json())
+   // .then(() => console.log("deleted!"));
+   .then(() => onDeleteItem(item));
+}
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button className={item.isInCart ? "remove" : "add"}
+      onClick={handleAddToCartClick}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
+        
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,10 +6,32 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
-
+useEffect(()=>{
+  fetch("http://localhost:4000/items")
+  .then ((response)=>response.json())
+  .then((items)=>setItems(items))
+},[])
+function handleAddItem(newItem){
+// console.log("In ShoppingList:", newItem)
+setItems([...items, newItem])
+}
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
+  function handleUpdateItem(updatedItem) {
+  const updatedItems = items.map((item) => {
+    if (item.id === updatedItem.id) {
+      return updatedItem;
+    } else {
+      return item;
+    }
+  });
+  setItems(updatedItems);
+}
+function handleDeleteItem(deletedItem) {
+  const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+  setItems(updatedItems);
+}
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -19,14 +41,16 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item}
+          onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem} />
         ))}
       </ul>
     </div>
